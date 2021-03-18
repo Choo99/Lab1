e223e92e9b43aa09364b1ae222bb9a8cf10fe98b
Choo99 <dikfong123@hotmail.com>
2021-03-18 14:43:27 +0800

    Modify location

---
+++ samples/src/demo/Location.java
@@ -0,0 +1,17 @@
+package demo;
+
+public class Location {
+
+	private int number;
+	private String name;
+	private String state;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	
+}
