b6d731158c71aac706a8e6a40bda7313fcca0267
Choo99 <dikfong123@hotmail.com>
2021-03-18 17:11:40 +0800

    Lab

---
+++ Lab1/src/exercise7/ConvertDemo.java
@@ -0,0 +1,13 @@
+package exercise7;
+
+public class ConvertDemo {
+
+	public static void main(String[] args) {
+		
+		Exercise7 convert = new Exercise7();
+		System.out.println("Human who has 70kg on Earth will become "
+		+ convert.calculateWeightOnMars(70)+ "kg on Mars");
+		
+	}
+
+}
