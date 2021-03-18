21f616b4ebf1cc16881cb456f4d4b290f8220a31
Choo99 <dikfong123@hotmail.com>
2021-03-18 16:27:20 +0800

    third commit

--- Lab1/src/excercise7/Excercise7.java
+++ Lab1/src/excercise7/Excercise7.java
@@ -30,11 +30,6 @@ public class Excercise7 {
 			return weightOnMars;
 		}
 		
-		public static void main(String[] args) {
-			
-			Excercise7 convert = new Excercise7();
-			System.out.println("Human who has 70kg on Earth will become " + convert.calculateWeightOnMars(70)+ "kg on Mars");
-			
-		}
+		
 	
 }
--- samples/src/demo/GitTest.java
+++ samples/src/demo/GitTest.java
@@ -3,7 +3,7 @@ package demo;
 public class GitTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.out.println("Git is fun");
 	}
 
